6214b1ec7d7fc5d488ab190b676e5969d7d9df80
William Raendchen <william.raendchen@gmail.com>
2025-05-30 00:00:00 +0200

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
 #[macro_use]
 extern crate rocket;
 
-use rocket::fs::{relative, FileServer, NamedFile};
-
 use std::env;
+
+use rocket::Config;
+use std::path::PathBuf;
+
+fn static_dir() -> PathBuf {
+    PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("static")
+}
 
 #[launch]
 fn rocket() -> _ {
-    let figment = rocket::Config::figment()
-        .merge(("port", env::var("PORT").unwrap_or_else(|_| "8083".into()).parse::<u16>().unwrap()));
+    let figment = Config::figment().merge((
+        "port",
+        env::var("PORT")
+            .unwrap_or_else(|_| "8000".into())
+            .parse::<u16>()
+            .unwrap(),
+    ));
 
-    rocket::custom(figment)
-        .mount("/", FileServer::from(relative!("static")).rank(0))
+    rocket::custom(figment).mount("/", rocket::fs::FileServer::from(static_dir()))
 }
